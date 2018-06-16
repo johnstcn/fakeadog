2bd639411afc248f956823bd138cca6ca0fbadb2
Cian Johnston <johnstcn@gmail.com>
2018-06-16 10:43:18 +0100

    reorg

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-CGO_ENABLED=0 GOOS=linux go build -a -ldflags '-w' .
+CGO_ENABLED=0 GOOS=linux go build  -ldflags '-w' github.com/johnstcn/fakeadog/cmd/fakeadog
 docker build -t johnstcn/fakeadog .
